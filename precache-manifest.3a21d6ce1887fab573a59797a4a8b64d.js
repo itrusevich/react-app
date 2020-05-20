f27789e59406baa09c2164cf029ba76d37553a59
Trusevich <ITrusevich@alluma.org>
2020-05-19 18:58:57 -0700

    Updates

---
+++ precache-manifest.3a21d6ce1887fab573a59797a4a8b64d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79445cabc037fc93cdad8397417310d8",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "26927b7fc9123a925c19",
+    "url": "/react-app/static/css/main.0acda547.chunk.css"
+  },
+  {
+    "revision": "c509ec726f99033e630d",
+    "url": "/react-app/static/js/2.c11c0722.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.c11c0722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26927b7fc9123a925c19",
+    "url": "/react-app/static/js/main.e0beddec.chunk.js"
+  },
+  {
+    "revision": "86f441ee41cdbf8cebdc",
+    "url": "/react-app/static/js/runtime-main.da6e56c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
